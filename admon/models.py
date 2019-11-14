3852064bbde40dce95e33bd3b8875e3713abeb6f
FranklinPerez <franklin9perez952012@gmail.com>
2019-11-14 10:19:26 -0600

    modelos de estudiante, materia, profesor, grado

--- admon/models.py
+++ admon/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import *
+from django.utils import timezone
 
 # Create your models here.
 #modelo para los usuarios del sistema
@@ -7,7 +9,7 @@ class Usuario(models.Model):
 	pasUsu=models.CharField(max_length=20,help_text="Ingrese contraseña, maximo 20 caracteres")
 
 	TIPO_USUARIO= (
-		('s','Secretaria'),
+		('s','Profesor'),
 		('m', 'Medico'),		
 		)
 
@@ -22,3 +24,37 @@ class Usuario(models.Model):
 		 return '{0}, {1},{2}'.format(self.codUsu, self.pasUsu, self.tipo_usuario)
 #fin modelo para los usuarios del sistema
 
+# modelo para los estudiantes del sistema
+class Estudiante(models.Model):
+	nieEst=models.IntegerField(max_length=10,help_text="Codigo del MINED asignado al estudiante",primary_key=True)
+	nomEst=models.CharField(max_length=20,help_text="Primer y/o segundo nombre del estudiante")
+	apeEst=models.CharField(max_length=20,help_text="Primer y/o segundo apellido del estudiante")
+	fecNac=models.DateField(null=False, blank=True)
+	graEst=models.ForeignKey('Grado', on_delete=models.SET_NULL, null=True)
+	nomPad=models.CharField(max_length=100,help_text="Nombre del padre")
+	nomMad=models.CharField(max_length=100,help_text="Nombre de la madre")
+	dirEst=models.CharField(max_length=100,help_text="Direccion del estudiante")
+	def __str__(self):
+		return self.nieEst
+#fin modelo para los estudiantes del sistema
+
+# modelo para los grados de educacion que cursan los estudiantes
+class Grado(models.Model):
+	nomGra=models.CharField(max_length=15,help_text="nombre del grado", primary_key=True)
+	nivGra=models.CharField(max_length=20,help_text="nivel del grado (primer ciclo, segundo ciclo, etc)")
+	seccion=models.CharField(max_length=1,help_text="Seccion del grado")
+#fin modelo para los grados de educacion que cursan los estudiantes
+
+# modelo para las materias que cursan los estudiantes
+class Materia(models.Model):
+	codMat=models.CharField(max_length=15,help_text="codigo de la materia", primary_key=True)
+	nomMat=models.CharField(max_length=15,help_text="nombre de la materia")
+	graMat=models.ForeignKey('Grado', on_delete=models.SET_NULL, null=True)
+	proMat=models.ForeignKey('Profesor', on_delete=models.SET_NULL, null=False)
+#fin modelo para los materias que cursan los estudiantes
+
+# modelo para los profesores
+class Profesor(models.Model):
+	codPro=models.CharField(max_length=15,help_text="codigo del profesor", primary_key=True)
+	nomPro=models.CharField(max_length=100,help_text="nombre del profesor")	
+#fin modelo para los profesores
